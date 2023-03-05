36411799524936357ce426c7da7a60830e03fa63
Rezoan Shakil Prince <prince.cuet.77@gmail.com>
2023-03-06 00:26:47 +0600

    Start working

--- src/index.js
+++ src/index.js
@@ -1,11 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+// import App from './App'
 
+const Greeting = () => {
+    return <h4>Hello world</h4>
+}
+
+// 1. Which component I want to render.
+// 2. Where I want to render
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+    <React.StrictMode>
+        {/* <App /> */}
+        <Greeting />
+    </React.StrictMode>,
+    document.getElementById('root')
+)
